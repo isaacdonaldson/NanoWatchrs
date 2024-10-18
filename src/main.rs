713c4a3de4510f9c0bbf89d5f51349cae9099b9c
Isaac <isaac.w.donaldson@gmail.com>
2024-10-18 15:31:00 -0700

    added history line rendering

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use chrono::NaiveDateTime;
+use chrono::{NaiveDate, NaiveDateTime};
 use minijinja::{context, path_loader, Environment};
 use serde::{Deserialize, Serialize};
 
+use std::fs;
+
+const ASSETS_PATH: &str = "assets";
 const CONFIG_PATH: &str = "examples/basic.json";
-const DATE_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
+const DATE_FORMAT: &str = "%Y-%m-%d";
+const LONG_DATE_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
 
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
@@ -35,9 +39,9 @@ pub struct StatusBlock {
     subtitle: String,
     status: String,
     state: State,
-    #[serde(with = "date_format")]
+    #[serde(with = "long_date_format")]
     updated_at: NaiveDateTime,
-    history_line: HistoryLine,
+    history_line: Vec<HistoryEntry>,
     uptime: String,
 }
 
@@ -51,7 +55,12 @@ pub enum State {
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct HistoryLine(pub String);
+pub struct HistoryEntry {
+    #[serde(with = "date_format")]
+    date: NaiveDate,
+    state: String,
+    notes: String,
+}
 
 fn main() -> Result<()> {
     let mut env = Environment::new();
@@ -75,7 +84,7 @@ fn main() -> Result<()> {
         status_blocks => config.status_blocks,
     };
 
-    println!("{}", template.render(context)?);
+    let _ = write_string_to_asset_folder("index.html", &template.render(context)?);
 
     // Example: Render a template from a subfolder
     // if let Ok(template) = env.get_template("subfolder/example.txt") {
@@ -93,17 +102,49 @@ fn read_config_file(config_path: &str) -> Result<StatusPageContext> {
     Ok(config)
 }
 
+fn write_string_to_asset_folder(file_name: &str, content: &str) -> Result<()> {
+    let full_path = format!("{}/{}", ASSETS_PATH, file_name);
+    fs::write(full_path, content)?;
+    Ok(())
+}
+
 mod date_format {
-    use chrono::NaiveDateTime;
+    use chrono::NaiveDate;
     use serde::{self, Deserialize, Deserializer, Serializer};
 
     use crate::DATE_FORMAT;
+
+    pub fn serialize<S>(date: &NaiveDate, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let s = format!("{}", date.format(DATE_FORMAT));
+        serializer.serialize_str(&s)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDate, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+
+        let dt = NaiveDate::parse_from_str(&s, DATE_FORMAT).map_err(serde::de::Error::custom)?;
+
+        Ok(dt)
+    }
+}
+
+mod long_date_format {
+    use chrono::NaiveDateTime;
+    use serde::{self, Deserialize, Deserializer, Serializer};
+
+    use crate::LONG_DATE_FORMAT;
 
     pub fn serialize<S>(date: &NaiveDateTime, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let s = format!("{}", date.format(DATE_FORMAT));
+        let s = format!("{}", date.format(LONG_DATE_FORMAT));
         serializer.serialize_str(&s)
     }
 
@@ -113,8 +154,8 @@ mod date_format {
     {
         let s = String::deserialize(deserializer)?;
 
-        let dt =
-            NaiveDateTime::parse_from_str(&s, DATE_FORMAT).map_err(serde::de::Error::custom)?;
+        let dt = NaiveDateTime::parse_from_str(&s, LONG_DATE_FORMAT)
+            .map_err(serde::de::Error::custom)?;
 
         Ok(dt)
     }

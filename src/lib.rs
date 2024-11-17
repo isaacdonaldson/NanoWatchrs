aada8dde88d68c16c58c446fda65808d95d84571
Isaac <isaac.w.donaldson@gmail.com>
2024-11-17 13:21:33 -0800

    use a lib file where some common types are defined

---
+++ src/lib.rs
@@ -0,0 +1,220 @@
+use chrono::{NaiveDate, NaiveDateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+pub mod utils;
+
+pub const ASSETS_PATH: &'static str = "assets";
+pub const CONFIG_PATH: &'static str = "examples/config.json";
+pub const DATE_FORMAT: &'static str = "%Y-%m-%d";
+pub const LONG_DATE_FORMAT: &'static str = "%Y-%m-%d %H:%M:%S";
+pub const HISTORY_PATH: &'static str = "examples/history.json";
+pub const HISTORY_LENGTH: usize = 30;
+
+pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct StatusPageContext {
+    pub settings: SiteSettings,
+    pub checks: Vec<Check>,
+    pub incidents: Vec<Incident>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct SiteSettings {
+    pub site: SiteParams,
+    pub page: PageParams,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct SiteParams {
+    pub name: String,
+    pub description: String,
+    pub url: String,
+    pub logo: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct PageParams {
+    pub title: String,
+    pub header: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct StatusBlock {
+    pub title: String,
+    pub subtitle: String,
+    pub status: String,
+    pub state: State,
+    #[serde(with = "long_date_format")]
+    pub updated_at: NaiveDateTime,
+    pub history_line: Vec<HistoryEntry>,
+    pub uptime: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum State {
+    #[serde(rename(deserialize = "unknown"))]
+    Disabled,
+    Success,
+    Warning,
+    Danger,
+    Failure,
+}
+
+impl State {
+    pub fn to_state(&self) -> &str {
+        match self {
+            State::Success => "success",
+            State::Warning => "warning",
+            State::Danger => "danger",
+            State::Failure => "failure",
+            State::Disabled => "disabled",
+        }
+    }
+
+    pub fn to_status(&self) -> &str {
+        match self {
+            State::Success => "OK",
+            State::Warning => "Degraded",
+            State::Danger => "Issues",
+            State::Failure => "Down",
+            State::Disabled => "Unknown",
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct HistoryEntry {
+    #[serde(with = "date_format")]
+    pub date: NaiveDate,
+    pub state: State,
+    pub notes: String,
+}
+
+impl HistoryEntry {
+    pub fn new_today(state: State, notes: String) -> Self {
+        Self {
+            date: Utc::now().naive_utc().date(),
+            state,
+            notes,
+        }
+    }
+
+    pub fn default_for_date(date: NaiveDate) -> Self {
+        Self {
+            date,
+            state: State::Disabled,
+            notes: String::from("Information N/A"),
+        }
+    }
+}
+
+impl Default for HistoryEntry {
+    fn default() -> Self {
+        Self {
+            date: Utc::now().naive_utc().date(),
+            state: State::Disabled,
+            notes: String::from("Information N/A"),
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "lowercase")]
+pub struct Check {
+    pub name: String,
+    pub description: Option<String>,
+    pub target: String,
+    pub page_link: Option<String>,
+    pub expected_status: Option<u16>,
+    pub timeout_ms: u64,
+    pub polling_interval: u64,
+    #[serde(rename = "type")]
+    pub check_type: CheckType,
+    pub port: Option<u16>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "lowercase")]
+pub enum CheckType {
+    Http,
+    Ping,
+    Port,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "lowercase")]
+pub enum CheckResult {
+    Success,
+    Failure,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "lowercase")]
+pub struct Incident {
+    pub title: String,
+    pub description: String,
+    pub status: String,
+    pub display_date: String,
+    #[serde(with = "long_date_format")]
+    pub started_at: NaiveDateTime,
+    #[serde(with = "long_date_format")]
+    pub resolved_at: NaiveDateTime,
+}
+
+pub mod date_format {
+    // This is for dates in the format of "2021-01-01"
+    use chrono::NaiveDate;
+    use serde::{self, Deserialize, Deserializer, Serializer};
+
+    use crate::DATE_FORMAT;
+
+    pub fn serialize<S>(date: &NaiveDate, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let s = format!("{}", date.format(DATE_FORMAT));
+        serializer.serialize_str(&s)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDate, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+
+        let dt = NaiveDate::parse_from_str(&s, DATE_FORMAT).map_err(serde::de::Error::custom)?;
+
+        Ok(dt)
+    }
+}
+
+pub mod long_date_format {
+    // This is for dates in the format of "2021-01-01 12:00:00"
+    use chrono::NaiveDateTime;
+    use serde::{self, Deserialize, Deserializer, Serializer};
+
+    use crate::LONG_DATE_FORMAT;
+
+    pub fn serialize<S>(date: &NaiveDateTime, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let s = format!("{}", date.format(LONG_DATE_FORMAT));
+        serializer.serialize_str(&s)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+
+        let dt = NaiveDateTime::parse_from_str(&s, LONG_DATE_FORMAT)
+            .map_err(serde::de::Error::custom)?;
+
+        Ok(dt)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,219 +1,135 @@
-use chrono::{NaiveDate, NaiveDateTime};
-use minijinja::{context, path_loader, Environment};
-use serde::{Deserialize, Serialize};
+use minijinja::context;
 
 use std::collections::HashMap;
-use std::fs;
 
-const ASSETS_PATH: &str = "assets";
-const CONFIG_PATH: &str = "examples/basic.json";
-const DATE_FORMAT: &str = "%Y-%m-%d";
-const LONG_DATE_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
-const HISTORY_LENGTH: usize = 30;
-
-pub type Error = Box<dyn std::error::Error>;
-pub type Result<T> = std::result::Result<T, Error>;
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct StatusPageContext {
-    site: SiteParams,
-    page: PageParams,
-    status_blocks: Vec<StatusBlock>,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct SiteParams {
-    name: String,
-    description: String,
-    url: String,
-    logo: String,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct PageParams {
-    title: String,
-    header: String,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct StatusBlock {
-    title: String,
-    subtitle: String,
-    status: String,
-    state: State,
-    #[serde(with = "long_date_format")]
-    updated_at: NaiveDateTime,
-    history_line: Vec<HistoryEntry>,
-    uptime: String,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-#[serde(rename_all = "lowercase")]
-pub enum State {
-    Success,
-    Warning,
-    Danger,
-    Failure,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct HistoryEntry {
-    #[serde(with = "date_format")]
-    date: NaiveDate,
-    state: String,
-    notes: String,
-}
+use nanowatchrs::utils::config::{read_config_file, read_history_file, HistorySection};
+use nanowatchrs::utils::templates::{
+    create_env, render_status_block, write_string_to_asset_folder,
+};
+use nanowatchrs::Result;
+use nanowatchrs::{CONFIG_PATH, HISTORY_PATH};
 
 fn main() -> Result<()> {
-    let mut env = Environment::new();
-    env.set_loader(path_loader("src/templates"));
+    let env = create_env();
 
     // Example: Render the "hello.txt" template
     let config = read_config_file(CONFIG_PATH)
         .expect(format!("Failed to read config file at '{}'", CONFIG_PATH).as_str());
 
-    let template = match env.get_template("index.html.jinja") {
-        Ok(template) => template,
-        Err(e) => {
-            println!("Template 'index.html.jijna' not found.");
-            return Err(Box::new(e));
-        }
-    };
-
-    let rendered_status_blocks = config
-        .status_blocks
+    let history = read_history_file(HISTORY_PATH)
+        .expect(format!("Failed to read history file at '{}'", HISTORY_PATH).as_str())
+        .watchers
         .iter()
-        .map(|block| {
-            let complete_events = generate_history(&block.history_line);
-            render_status(&env, block.clone(), complete_events)
+        .map(|e| (e.name.clone(), e.clone()))
+        .collect::<HashMap<String, HistorySection>>();
+
+    let template = env.get_template("index.html.jinja")?;
+
+    // println!("Config: {:#?}", config);
+    // println!("History: {:#?}", history);
+
+    let status_blocks: Option<String> = config
+        .checks
+        .iter()
+        .filter_map(|check| match history.get(&check.name) {
+            None => {
+                println!(
+                    "History entry for '{}' is required but not found",
+                    check.name
+                );
+                None
+            }
+            Some(history) => {
+                // println!("Rendering status block for '{}'", check.name);
+                // println!("{:#?}", history);
+                render_status_block(&env, check, history.clone()).ok()
+            }
         })
-        .collect::<Result<Vec<String>>>()?
-        .join("\n");
+        .reduce(|a, b| format!("{}\n{}", a, b));
+
+    if status_blocks.is_none() {
+        return Err("Error rendering status blocks".into());
+    }
 
     let context = context! {
-        site => config.site,
-        page => config.page,
-        status_blocks => config.status_blocks,
-        rendered_status_blocks => rendered_status_blocks,
+        site => config.settings.site,
+        page => config.settings.page,
+        incidents => format!("{:#?}", config.incidents),
+        // history => history,
+        rendered_blocks => status_blocks.unwrap(),
     };
 
     let _ = write_string_to_asset_folder("index.html", &template.render(context)?);
 
+    // let rendered_status_blocks = config
+    //     .status_blocks
+    //     .iter()
+    //     .map(|block| {
+    //         let complete_events = generate_history(&block.history_line);
+    //         render_status(&env, block.clone(), complete_events)
+    //     })
+    //     .collect::<Result<Vec<String>>>()?
+    //     .join("\n");
+
+    // let context = context! {
+    //     site => config.site,
+    //     page => config.page,
+    //     status_blocks => config.status_blocks,
+    //     rendered_status_blocks => rendered_status_blocks,
+    // };
+
+    // let _ = write_string_to_asset_folder("index.html", &template.render(context)?);
+
     Ok(())
 }
 
-fn read_config_file(config_path: &str) -> Result<StatusPageContext> {
-    let config_file = std::fs::read_to_string(config_path)?;
-    let config: StatusPageContext = serde_json::from_str(&config_file)?;
-    Ok(config)
-}
+// fn generate_history(events: &Vec<HistoryEntry>) -> Vec<HistoryEntry> {
+//     let today = chrono::Local::now().date_naive();
+//     let mut history_map: HashMap<NaiveDate, HistoryEntry> = events
+//         .iter()
+//         .map(|entry| (entry.date, entry.clone()))
+//         .collect();
 
-fn write_string_to_asset_folder(file_name: &str, content: &str) -> Result<()> {
-    let full_path = format!("{}/{}", ASSETS_PATH, file_name);
-    fs::write(full_path, content)?;
-    Ok(())
-}
+//     let mut result = Vec::with_capacity(HISTORY_LENGTH);
 
-fn generate_history(events: &Vec<HistoryEntry>) -> Vec<HistoryEntry> {
-    let today = chrono::Local::now().date_naive();
-    let mut history_map: HashMap<NaiveDate, HistoryEntry> = events
-        .iter()
-        .map(|entry| (entry.date, entry.clone()))
-        .collect();
+//     for days_ago in 0..HISTORY_LENGTH {
+//         let date = today - chrono::Duration::days(days_ago as i64);
+//         let entry = history_map.entry(date).or_insert_with(|| HistoryEntry {
+//             date,
+//             state: State::Success,
+//             notes: "No Incident".to_string(),
+//         });
+//         result.push(entry.clone());
+//     }
 
-    let mut result = Vec::with_capacity(HISTORY_LENGTH);
+//     result.reverse(); // To have the oldest date first
+//     result
+// }
 
-    for days_ago in 0..HISTORY_LENGTH {
-        let date = today - chrono::Duration::days(days_ago as i64);
-        let entry = history_map.entry(date).or_insert_with(|| HistoryEntry {
-            date,
-            state: "success".to_string(),
-            notes: "No Incident".to_string(),
-        });
-        result.push(entry.clone());
-    }
+// fn render_status(
+//     env: &Environment,
+//     block: StatusBlock,
+//     complete_events: Vec<HistoryEntry>,
+// ) -> Result<String> {
+//     let template = match env.get_template("partials/status.html.jinja") {
+//         Ok(template) => template,
+//         Err(e) => {
+//             println!("Template 'partials/status.html.jijna' not found.");
+//             return Err(Box::new(e));
+//         }
+//     };
 
-    result.reverse(); // To have the oldest date first
-    result
-}
+//     let ctx = context! {
+//         title => block.title,
+//         subtitle => block.subtitle,
+//         status => block.status,
+//         state => block.state,
+//         updated_at => block.updated_at,
+//         uptime => block.uptime,
+//         history_line => complete_events,
+//     };
 
-fn render_status(
-    env: &Environment,
-    block: StatusBlock,
-    complete_events: Vec<HistoryEntry>,
-) -> Result<String> {
-    let template = match env.get_template("partials/status.html.jinja") {
-        Ok(template) => template,
-        Err(e) => {
-            println!("Template 'partials/status.html.jijna' not found.");
-            return Err(Box::new(e));
-        }
-    };
-
-    let ctx = context! {
-        title => block.title,
-        subtitle => block.subtitle,
-        status => block.status,
-        state => block.state,
-        updated_at => block.updated_at,
-        uptime => block.uptime,
-        history_line => complete_events,
-    };
-
-    template
-        .render(ctx)
-        .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
-}
-
-mod date_format {
-    use chrono::NaiveDate;
-    use serde::{self, Deserialize, Deserializer, Serializer};
-
-    use crate::DATE_FORMAT;
-
-    pub fn serialize<S>(date: &NaiveDate, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let s = format!("{}", date.format(DATE_FORMAT));
-        serializer.serialize_str(&s)
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDate, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-
-        let dt = NaiveDate::parse_from_str(&s, DATE_FORMAT).map_err(serde::de::Error::custom)?;
-
-        Ok(dt)
-    }
-}
-
-mod long_date_format {
-    use chrono::NaiveDateTime;
-    use serde::{self, Deserialize, Deserializer, Serializer};
-
-    use crate::LONG_DATE_FORMAT;
-
-    pub fn serialize<S>(date: &NaiveDateTime, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let s = format!("{}", date.format(LONG_DATE_FORMAT));
-        serializer.serialize_str(&s)
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-
-        let dt = NaiveDateTime::parse_from_str(&s, LONG_DATE_FORMAT)
-            .map_err(serde::de::Error::custom)?;
-
-        Ok(dt)
-    }
-}
+//     template
+//         .render(ctx)
+//         .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
+// }
